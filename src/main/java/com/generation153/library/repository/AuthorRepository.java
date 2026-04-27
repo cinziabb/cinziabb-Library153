bdb21844e1231d0e0cf8857fc700742bf688b28b
Alessandro Fruci <ale.fruci@outlook.com>
2026-04-27 18:04:27 +0200

    Prime query create nell'AuthorRepository

--- src/main/java/com/generation153/library/repository/AuthorRepository.java
+++ src/main/java/com/generation153/library/repository/AuthorRepository.java
@@ -1,9 +1,18 @@
 package com.generation153.library.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation153.library.entity.Author;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
-
+	
+	List<Author> findByFirstnameAndLastnameIgnoreCase(String firstNname, String lastName);
+	
+	
+	List<Author> findByLastnameIgnoreCase(String lastName);
+	
+	List<Author> findByLastnameStartingWith(String lastName); 
+	
 }
