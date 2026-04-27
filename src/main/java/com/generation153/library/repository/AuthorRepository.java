b07ad40e80a621c3a3397dc64f204fa8e6b81c18
Andrea Lamicela <andrealamicela9996@gmail.com>
2026-04-27 18:05:40 +0200

    Prime query di Author Repository

--- src/main/java/com/generation153/library/repository/AuthorRepository.java
+++ src/main/java/com/generation153/library/repository/AuthorRepository.java
@@ -1,9 +1,21 @@
 package com.generation153.library.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation153.library.entity.Author;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+	
+	List<Author> findByFirstNameAndLastNameIgnoreCase(String firstName, String lastName);
+	
+	List<Author> findByLastNameIgnoreCase(String lastName);
+	
+	List<Author> findByLastNameStartingWith(String lastName);
+	
+	
+	
+	
 
 }
