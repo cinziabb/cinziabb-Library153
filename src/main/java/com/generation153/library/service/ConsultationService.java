7de52cb9257ad141182a06a2fdd0e3d880495f2c
Gabriel Roncolato <gabrielroncolato0007@gmail.com>
2026-04-28 17:17:58 +0200

    Nuova classe

---
+++ src/main/java/com/generation153/library/service/ConsultationService.java
@@ -0,0 +1,11 @@
+package com.generation153.library.service;
+
+//vedere quali libri sono attualmente disponibili
+//vedere quali libri sono più richiesti o più prestati
+//visualizzare i libri attualmente in prestito
+//visualizzare i prestiti effettuati da uno specifico utente
+
+
+public interface ConsultationService {
+	
+}
