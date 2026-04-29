2c60d0d2a4f4038d8097d58f3479836a9e5501ca
Gabriel Roncolato <gabrielroncolato0007@gmail.com>
2026-04-29 12:13:35 +0200

    ideaziene query su ConsultationService

--- src/main/java/com/generation153/library/repository/ConsultationRepository.java
+++ src/main/java/com/generation153/library/repository/ConsultationRepository.java
@@ -3,6 +3,7 @@ package com.generation153.library.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.generation153.library.entity.Consultation;
 import com.generation153.library.entity.Copy;
@@ -14,4 +15,11 @@ public interface ConsultationRepository extends JpaRepository<Consultation, Inte
 	List<Consultation> findByCopy(Copy copy);
 	List<Consultation> findByEndTimeIsNull();
 	List<Consultation> findByEndTimeIsNotNull();
+	@Query ("""
+			SELECT c.copy.book.title, COUNT(c)
+			FROM Consultation c
+			GROUP BY c.copy.book.title
+			ORDER BY COUNT(c) DESC
+			""")
+	List<Object[]> findMostConsulted();
 }
--- src/main/java/com/generation153/library/service/ConsultationService.java
+++ src/main/java/com/generation153/library/service/ConsultationService.java
@@ -28,6 +28,19 @@ public interface ConsultationService {
 	List<Consultation> findActiveConsultation();
 	List<Consultation> findCompletedConsultation();
 	
-	
+	List<Object[]> findMostWantedBook();
+	/*
+	 * SELECT DISTINCT b.title
+	 * FROM consultation co
+	 * JOIN copy c ON co.copy_id = c.id
+	 * JOIN book b ON c.book_id = b.id
+	 * WHERE c.available = false
+	 * ORDER BY b.title;
+	 * 
+	 * SELECT DISTINCT c.copy.book.title
+	 * FROM Consultation c
+	 * WHERE c.copy.available
+	 * ORDER BY c.copy.book.title
+	 */
 	
 }
