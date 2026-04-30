b7ff3bad9c9d57b11f881d4deac710ec4808be66
Bocchi Cinzia <bocchicinzia@gmail.com>
2026-04-30 11:45:30 +0200

    Aggiunta bean validation ai metodi del BookController

--- src/main/java/com/generation153/library/controller/BookController.java
+++ src/main/java/com/generation153/library/controller/BookController.java
@@ -8,12 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation153.library.dto.BookCreateDTO;
 import com.generation153.library.entity.Book;
 import com.generation153.library.service.BookService;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -42,13 +46,13 @@ public class BookController {
 
 	// API: GET + http://localhost:8080/api/v1/admin/books/1 
 	@GetMapping("/admin/books/{id}")
-	public ResponseEntity<Book> getBookById(@PathVariable Integer id) {
+	public ResponseEntity<Book> getBookById(@Min(1) @PathVariable Integer id) {
 		return new ResponseEntity<>(bookService.findBookById(id), HttpStatus.OK);
 	}
 	
 	// API: POST + http://localhost:8080/api/v1/admin/books 
 	@PostMapping("/admin/books")
-	public ResponseEntity<Book> createBook(BookCreateDTO dto) {
+	public ResponseEntity<Book> createBook(@Valid @RequestBody BookCreateDTO dto) {
 		
 		// mapping dto --> entity
 		Book book = modelMapper.map(dto, Book.class);
